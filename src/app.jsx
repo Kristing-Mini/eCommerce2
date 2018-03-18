8073b0d880d40ef937cad09b7bb4b30abcda7102
Kristing-Mini <1140744631@qq.com>
2018-03-18 19:23:50 +0800

    first-modify

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import './index.scss';
+
+ReactDom.render(
+	<div>
+	<i className="fa fa-address-book"></i>
+	<h2>wait</h2>
+	<h3>yes</h3>
+	<h2>oh</h2>
+    <h1>hello.world~~</h1>
+    </div>,
+    document.getElementById('app')
+);
